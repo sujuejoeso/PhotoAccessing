875c6a2dea2ea155100819bbdfcd967bee9ad9be
sujuejoeso <sujuechenhao@yahoo.com>
2020-02-21 13:18:11 +1030

    ini

--- app/src/main/java/com/joeso/photoaccessing/MainActivity.java
+++ app/src/main/java/com/joeso/photoaccessing/MainActivity.java
@@ -2,13 +2,44 @@ package com.joeso.photoaccessing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    public static final int REQUEST_PICK_IMAGE = 11101;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent,"Selected Photo"),0);
+            }
+        });
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        base.OnActivityResult(requestCode, resultCode, data);
+        if (resultCode == Result)
+        {
+            var imageView = FindViewById<ImageView>(Resource.Id.myImageView);
+            imageView.SetImageURI(data.Data);
+        }
+
+
     }
 }
